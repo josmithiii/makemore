9977004698cdbb7f5a82fb3ced284dd67d39ffc1
Julius Smith <julius.smith@gmail.com>
2024-01-25 18:53:22 -0800

    jos: add some tests

---
+++ tests/tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+# USAGE:
+# In PARENT DIRECTORY, say
+#   sh tests/tests.sh
+
+MAKEMORE=makemore.py
+
+DATADIR=data
+LISTOPSDIR=$DATADIR/listops
+WORDSDIR=$DATADIR/words
+DISTANCEDIR=$DATADIR/distance
+
+WORDS_DATA="$WORDSDIR/names.txt"
+LISTOPS_DATA="$LISTOPSDIR/train_d8.tsv"
+DISTANCE_DATA="DISTANCEDIR/dist1.txt"
+
+python $MAKEMORE --input "$WORDS_DATA" --type gru --max-steps 210 && \
+python $MAKEMORE --input "$WORDS_DATA" --data-mode "words" --type transformer --max-steps 210 && \
+python $MAKEMORE --input "$WORDS_DATA" --type mamba --max-steps 210 && \
+python $MAKEMORE --input "$LISTOPS_DATA" --data-mode "qa" --block-size 64 --type gru --max-steps 210 && \
+python $MAKEMORE --input "$LISTOPS_DATA" --data-mode "qa" --block-size 64 --type transformer --max-steps 210 && \
+python $MAKEMORE --input "$DISTANCE_DATA" --data-mode "distance" --batch-size 8 --block-size 32 --type gru --max-steps 210 && \
+say All tests completed with error code 0 && \
+say complete || \
+say failed
